3c647ad938a08a17309c2bc977feac3dab4b31f4
Dmitriy Larionov <larionov.d.y@gmail.com>
2022-01-23 22:16:19 +0300

    Добавьте файлы проекта.

---
+++ WpfHomeWork19/Models/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHomeWork19.Models
+{
+    public static class Circle
+    {
+        public const double PI = 3.1415926535897931;
+        //метод возвращает значение длины окружности
+        public static double Length(double radius) => Math.Round(2 * PI * radius, 2);
+        //метод возвращает значение площади круга
+        public static double Area(double radius) => Math.Round(Math.Pow(radius, 2) * PI, 2);
+    }
+}
+
---
+++ WpfHomeWork19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfHomeWork19.Models;
+
+namespace WpfHomeWork19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double area;
+        public double Area
+        {
+            get => area;
+            set
+            {
+                area = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = Circle.Length(Radius);
+            Area = Circle.Area(Radius);            
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            return true;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
